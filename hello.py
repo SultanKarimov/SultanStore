532fab898af5f1b69cb79084b11842da5a1f3869
sultan_karimov <su-30.1998@mail.ru>
2022-02-08 22:14:40 +0300

    updated

--- hello.py
+++ hello.py
@@ -1,2 +1,46 @@
+# import os
+# while True:
+#     sayt = input('Введите адрес сайта:\n')
+#     if sayt == 'завершить':
+#         break
 
-print('hello world and Sultan')
+#     if 'https://' in sayt:
+#         os.system('start ' + sayt)
+#     elif 'www.' in sayt:
+#         sayt = 'https://' + sayt
+#         os.system('start ' + sayt)
+#     else:
+#         sayt = 'https://www.' + sayt
+#         os.system('start ' + sayt)
+
+
+# x =''
+# while len(x)<5:
+#     y = input('Ввод  данных: ')
+#     if y == 'o':
+#         continue
+#     if y == 'l':
+#         break
+#     x+=y
+# else: 
+#     print(x)
+
+
+
+# m = [1, 2, 3]
+# k=[]
+# for i in list(m):
+#     k.append(i*2)
+# print(k)
+
+
+
+# n = list(range(10))
+# m=[]
+
+# for i in n:
+#     if i == 8:
+#         continue
+#     m +=[i]
+# else:
+#     print(m)
