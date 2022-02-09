f97080a168fca80c4b93e70977ca0148875c6e38
sultan_karimov <su-30.1998@mail.ru>
2022-02-09 20:21:42 +0300

    commited

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@
 #     sayt = input('Введите адрес сайта:\n')
 #     if sayt == 'завершить':
 #         break
-
 #     if 'https://' in sayt:
 #         os.system('start ' + sayt)
 #     elif 'www.' in sayt:
@@ -13,6 +12,8 @@
 #         sayt = 'https://www.' + sayt
 #         os.system('start ' + sayt)
 
+
+#Цикл WHILE
 
 # x =''
 # while len(x)<5:
@@ -27,6 +28,8 @@
 
 
 
+#Цикл FOR и Списки
+
 # m = [1, 2, 3]
 # k=[]
 # for i in list(m):
@@ -35,12 +38,75 @@
 
 
 
+#альтернатива способу выше:
+# m=[1,2,3]
+# for i in range(len(m)):
+#     m[i]+=3
+# print(m)
+
+
+
 # n = list(range(10))
 # m=[]
-
 # for i in n:
 #     if i == 8:
 #         continue
 #     m +=[i]
 # else:
 #     print(m)
+
+
+
+# n = list(range(10))
+# m=[]
+# for i in n:
+#     if i == 8:
+#         continue
+#     m.append(i)
+# else:
+#     print(m)
+
+
+
+
+# n = list(range(1,21))
+# m=[]
+# for i in n:
+#     if i % 2 == 0: #если i четное число, то в спискок m добавляем четное число, а из списка n удаляем
+#         m.append(i)
+#         n.remove(i)
+# else:
+#     print(n)
+#     print(m)
+  
+
+
+# n = list(range(1,21))
+# b= n[0::2] #аналогичный способ предыдущему создания нечетного списка чисел [start:stop:step]
+# print(b)
+
+
+
+#Кортежи Tuple
+
+# x=(9,8,7,6)
+# y =[]
+# for i in range(len(x)):
+#     y.append(x[i]+3)
+# print(y)
+
+
+
+# h=(1,2,3)
+# g=h
+# h +=(4,5)
+# print(h) #(1, 2, 3, 4, 5)
+# print(g) #(1, 2, 3) в отличие от списка кортеж не перезаписывается
+
+
+#практическое задание
+
+import os
+for i in os.walk('C:\\Users\sultan.karimov\Desktop\test3') :
+    print(i)
+    break
